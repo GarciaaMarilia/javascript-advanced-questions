ecdf652db2f6d8586de56a1dd3b8be02030edfa5
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2024-12-13 17:08:51 +0100

    capitalize

---
+++ capitalize.js
@@ -0,0 +1,10 @@
+function capitalize(string) {
+ let words = [];
+
+ for (let word of string.split(" ")) {
+  words.push(word[0].toUpperCase() + word.slice(1));
+ }
+ return words.join(' ');
+}
+
+console.log(capitalize("a short sentence"));
